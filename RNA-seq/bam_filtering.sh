b801c645229df606a39466162ca7f9b3961c496c
Elke Voeten <elkevoeten@icloud.com>
2025-03-21 14:14:42 +0100

    Update bam_filtering.sh

--- RNA-seq/bam_filtering.sh
+++ RNA-seq/bam_filtering.sh
@@ -10,7 +10,7 @@
 #SBATCH --error=/scratch/leuven/362/vsc36201/RNA-seq/logs/filtering_%j.err
 #SBATCH -A lp_svbelleghem      
 
-# Load the SAMtools module
+# Load modules
 module load cluster/genius/batch
 module load SAMtools/1.18-GCC-12.3.0
 module load libdeflate/1.19-GCCcore-13.2.0
@@ -23,14 +23,16 @@ for bamfile in "$bam_dir"/*.Aligned.sortedByCoord.out.bam; do
     # Extract the sample name by removing the file extension
     sample_name=$(basename "$bamfile" .Aligned.sortedByCoord.out.bam)
     
-    # Construct the output filtered BAM file path
-    output_bam="${bam_dir}/${sample_name}.sorted.filtered.bam"
+    # Construct the output filtered and sorted BAM file paths
+    filtered_bam="${bam_dir}/${sample_name}.filtered.bam"
+    sorted_bam="${bam_dir}/${sample_name}.filtered.sorted.bam"
     
-    # Run samtools view command to filter the BAM files
-    samtools view -f 0x02 -q 30 -b "$bamfile" > "$output_bam"
+    # Run samtools to filter and then sort the BAM files
+    samtools view -f 0x02 -q 30 -b "$bamfile" > "$filtered_bam"
+    samtools sort -o "$sorted_bam" "$filtered_bam"
     
     # Print a message when done processing each file
-    echo "Filtered $bamfile -> $output_bam"
+    echo "Filtered and sorted $bamfile -> $sorted_bam"
 done
 
 echo "Filtering complete for all BAM files."
